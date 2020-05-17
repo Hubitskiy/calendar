7ef17101469a3b3f09f9aed7a8698788e77b2437
Hubitskiy <dmitry.g@upservice.io>
2020-05-17 17:33:01 +0300

    add minor fix

---
+++ apps/event/utils/date_send_utils.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+
+class DateSendInvitation:
+
+    def __init__(self, validated_data):
+        self.validated_data = validated_data
+
+    def __call__(self, *args, **kwargs):
+        time_period = self.validated_data["time_period"]
+        event_date = self.validated_data["event_date"]
+        _time_period = {
+                'HOUR': event_date - timedelta(hours=1),
+                'DAY':  event_date - timedelta(days=1),
+                'WEEK': event_date - timedelta(weeks=1),
+            }.get(time_period, event_date)
+
+        self.validated_data["date_to_send_invitations"] = _time_period
+
+        return _time_period
--- apps/event/views.py
+++ apps/event/views.py
@@ -3,8 +3,7 @@ from .serializer import CreateEventSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from .event_services import DateSendInvitationService
-from logging import warning
+from .utils import DateSendInvitation
 
 
 class EventCreateView(CreateAPIView):
@@ -14,23 +13,17 @@ class EventCreateView(CreateAPIView):
 
     def _perform_create(self, serializer):
         validated_data = serializer.validated_data
-        dates = DateSendInvitationService.get_time_and_event_date(validated_data)
-        validated_data['date_to_send_invitations'] = dates()
+        add_date_to_send_invitation = DateSendInvitation(validated_data)
+        add_date_to_send_invitation()
         validated_data['user'] = self.request.user
         serializer.save()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        validate = serializer.is_valid()
-
-        if validate is not True:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
-
+        serializer.is_valid(raise_exception=True)
         self._perform_create(serializer)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def post(self, request, *args, **kwargs):
         return self.create(request)
-
-
