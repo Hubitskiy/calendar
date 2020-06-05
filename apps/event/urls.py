5bbd62f2a7650e8236f669a94b10e4eb7c885948
Hubitskiy <dmitry.g@upservice.io>
2020-06-05 23:51:51 +0300

    list view

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EventCreateView, RetrieveView
+from .views import EventCreateView, RetrieveView, ListView
 
 
 urlpatterns = [
     path('create/', EventCreateView.as_view()),
-    path('<int:pk>/', RetrieveView.as_view())
+    path('<int:pk>/', RetrieveView.as_view()),
+    path('', ListView.as_view({'get': 'list'}))
 ]
--- apps/event/views.py
+++ apps/event/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 from .serializer import CreateEventSerializer, RetrieveEventSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -18,12 +20,13 @@ class EventCreateView(CreateAPIView):
         get_date_to_send_invitation = DateSendInvitation(validated_data)
         validated_data["user"] = self.request.user
         validated_data["date_to_send_invitations"] = get_date_to_send_invitation()
-        serializer.save()
+        return serializer.save()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self._perform_create(serializer)
+        created_event = self._perform_create(serializer)
+        serializer = RetrieveEventSerializer(created_event)
         headers = self.get_success_headers(serializer.data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
@@ -37,3 +40,15 @@ class RetrieveView(RetrieveAPIView):
     serializer_class = RetrieveEventSerializer
     permission_classes = [IsAuthenticated, IsSelfUser]
     queryset = Event.objects
+
+
+class ListView(ListModelMixin, GenericViewSet):
+
+    serializer_class = RetrieveEventSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Event.objects
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(user_id=request.user.id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
